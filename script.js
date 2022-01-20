51f769dc6cf402e7cc6083743946d4392e3a9ff4
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-20 18:13:39 +0100

    response ok

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const submitButton = document.getElementById('submitButton');
 
+const getMovies = async (request) => {
+  try {
+    const response = await window.fetch(request);
+    const movies = await response.json();
+    console.log(movies);
+    } catch (error) {
+      console.log('Response error :s : ', error.message);
+  }  
+}
+
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
   const keyString = document.getElementById('keywords').value;
   const requestString = keyString.replace(/ /g, '+');
-  
+  const request = 'http://www.omdbapi.com/?apikey=55c79898&s=' + requestString;
+  console.log(request);
+  getMovies(request);
 });
+
