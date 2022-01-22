132372450207b44a239a369695f42e373120efcc
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-22 03:32:01 +0100

    organize movies

--- script.js
+++ script.js
@@ -14,10 +14,6 @@ const showMovies = () => {
   getMovies(requestUrl).then( movies => {
     movies.forEach( movie => {
       displayMovie(movie);
-      getMovieDetailed(movie.imdbID).then( movieDetailed => { 
-        createModal(movieDetailed);
-        createModalListener(movieDetailed.imdbID); 
-      });
     });
   });
 };
@@ -45,7 +41,7 @@ const getMovies = async (requestUrl) => {
 const getMovieDetailed = async (movieID) => {
   const request = urlStart + '&i=' + movieID;
   try {
-    const response = await window.fetch(request);
+    const response = await fetch(request);
     const detailedMovie = await response.json();
     return detailedMovie;
   }catch (error) { 
@@ -56,55 +52,55 @@ const getMovieDetailed = async (movieID) => {
 // ASYNC HTTP REQUEST
 const get = async (url) => {
   try {
-    const response = await window.fetch(url);
+    const response = await fetch(url);
     return await response.json();
   }catch (error) { 
     console.error('Error fetching data : ', error.message); 
   }
 };
 
-// Creates the HTML card elements for a movie
+// Creates the HTML movie element and modal and modal listener
 const displayMovie = (movie) => {
   main.innerHTML += `
-  <div id="movieCard__${movie.imdbID}" class="col-6 m-5">
+  <div id="movieCard__${movie.imdbID}" class="col-md-5 m-md-5 col-10 my-3">
     <div class="row">
-      <div class="col-2 mx-5 p-0">
-        <img src="${movie.Poster}" class="img-fluid rounded-start" alt="poster">
+      <div class="col-md-2 mx-md-5 p-0 col-7 mx-auto">
+        <img src="${movie.Poster}" class="img-fluid rounded" alt="">
       </div>
-      <div class="movie col d-flex flex-column justify-content-center align-items-center">
+      <div class="movie col my-3 p-3 d-flex flex-column justify-content-center align-items-center">
         <h6>${movie.Title}</h6>
         <p class="text-muted">${movie.Year}</p>
-        <button id="button__${movie.imdbID}" class="btn btn-success mt-3 px-5">Read more</button>
+        <button id="button__${movie.imdbID}" class="btn btn-outline-dark mt-1 px-5">Details</button>
       </div>
     </div>
   </div>
   `;
+  getMovieDetailed(movie.imdbID).then(movieDetailed => { 
+    createModal(movieDetailed);
+  });
 };
 
 // Adds hidden modals to a movie
 const createModal =  (movie) => {
-  try {
-    const movieCard = document.getElementById('movieCard__' + movie.imdbID);
-    movieCard.innerHTML += `
-      <div id="modal__${movie.imdbID}" class="modal container-fluid">
-        <div class="modal-content">
-          <div class="row">
-            <div class="col-3 text-center">
-              <img src="${movie.Poster}" class="img-fluid rounded-start" alt="poster">
-            </div>
-            <div class="col-6 px-5 align-self-center">
-              <p>${movie.Plot}</p>
-            </div>
-            <div class="col-3 text-center">
-              <span id="span__${movie.imdbID}" class="close m-5">&times;</span>
-            </div>
+  const movieCard = document.getElementById('movieCard__' + movie.imdbID);
+  movieCard.innerHTML += `
+    <div id="modal__${movie.imdbID}" class="modal container-fluid">
+      <div class="modal-content">
+        <div class="row">
+          <div class="col-3 text-center">
+            <img src="${movie.Poster}" class="img-fluid rounded" alt="">
+          </div>
+          <div class="col-6 px-5 align-self-center">
+            <p>${movie.Plot}</p>
+          </div>
+          <div class="col-3 text-center">
+            <span id="span__${movie.imdbID}" class="close m-5">&times;</span>
           </div>
         </div>
       </div>
-    `;
-  }catch (error) {
-    console.log('Response error :s : ', error.message);
-  }
+    </div>
+  `;
+  createModalListener(movie.imdbID);
 };
 
 const createModalListener = (id) => {
